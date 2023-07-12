16dd77c3f4d40585a0fb4d0f7041a7ebf725fb65
Guillaume <rossetguillaume759@gmail.com>
2023-07-13 00:04:24 +0200

    page login

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,25 +1,25 @@
 //gestion de l'affichage des catégories + la catégorie tous
 
-fetch("http://localhost:5678/api/categories")
-  .then((data) => data.json())
-  .then((cats) => {
-    const categorie = document.querySelector(".categorie");
-    let tous = document.createElement("span");
-    tous.classList.add("spantous");
-    tous.textContent = "Tous";
-    let parentDiv = tous.parentNode;
-    categorie.appendChild(tous);
-    for (const cat of cats) {
-      let span = document.createElement("span");
-      span.textContent = cat.name;
-      categorie.appendChild(span);
-    }
-  });
-
 // gestion de l'affichage des images avec le titre
 fetch("http://localhost:5678/api/works")
   .then((data) => data.json())
   .then((works) => {
+    fetch("http://localhost:5678/api/categories")
+      .then((data) => data.json())
+      .then((cats) => {
+        const categorie = document.querySelector(".categorie");
+        let tous = document.createElement("span");
+        tous.classList.add("spantous");
+        tous.textContent = "Tous";
+        let parentDiv = tous.parentNode;
+        categorie.appendChild(tous);
+        for (const cat of cats) {
+          let span = document.createElement("span");
+          span.textContent = cat.name;
+          categorie.appendChild(span);
+        }
+      });
+
     const gallery = document.querySelector(".gallery");
 
     for (const work of works) {
@@ -31,5 +31,12 @@ fetch("http://localhost:5678/api/works")
       gallery.appendChild(figure);
       figure.appendChild(img);
       figure.appendChild(figcaption);
+      var category = [works];
     }
+    var tableau = new Array();
+    tableau = [category[1]];
+    let objets = (category.name = "Objets");
+    console.log(tableau);
+    let test = [works.length];
+    console.log(test);
   });
